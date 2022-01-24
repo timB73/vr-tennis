a315aadb9adabda78d68498e00dda95c00ec0fbf
Tim Bartrum <timbartrum73@gmail.com>
2022-01-24 19:45:11 +0000

    Add topspin and backspin controls

---
+++ VR Tennis/Assets/Scripts/BallController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallController : MonoBehaviour
+{
+
+    [SerializeField] private int maxRotation = 100;
+    [SerializeField] private int accelerationFactor = 10;
+    [SerializeField] private int decellerationFactor = 2;
+
+    private int topspinRotation = 0;
+
+    private int backspinRotation = 0;
+
+    private Rigidbody ballRigidBody;
+
+    private Vector3 m_EulerAngleVelocity;
+
+    void Update()
+    {
+        if (Keyboard.current.wKey.isPressed && topspinRotation < maxRotation)
+        {
+            Debug.Log("BallController keyboard w pressed");
+            topspinRotation += accelerationFactor;
+        }
+        else if (!Keyboard.current.wKey.isPressed && topspinRotation > 0)
+        {
+            topspinRotation -= decellerationFactor;
+        }
+
+        if (Keyboard.current.sKey.isPressed && backspinRotation < maxRotation)
+        {
+            backspinRotation += accelerationFactor;
+        }
+        else if (!Keyboard.current.sKey.isPressed && backspinRotation > 0)
+        {
+            backspinRotation -= decellerationFactor;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRigidBody = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        int xRotation = topspinRotation - backspinRotation;
+
+        Debug.Log("X rotation: topspin (" + topspinRotation + ") - backspin (" + backspinRotation + ") = " + xRotation);
+
+        // add topspin
+        m_EulerAngleVelocity = new Vector3(xRotation, 0, 0); // rotate about x axis
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime);
+        ballRigidBody.MoveRotation(ballRigidBody.rotation * deltaRotation);
+    }
+}
