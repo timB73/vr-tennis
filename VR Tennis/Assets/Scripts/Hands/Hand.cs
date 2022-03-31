f08f86de5999c1797256bebcf2ade92b388bf138
Tim <timbartrum73@gmail.com>
2022-03-31 16:21:34 +0100

    Hand working with physics

--- VR Tennis/Assets/Scripts/Hands/Hand.cs
+++ VR Tennis/Assets/Scripts/Hands/Hand.cs
@@ -10,7 +10,7 @@ https://youtu.be/DxKWq7z4Xao
 [RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
 {
-    public float speed;
+    public float animationSpeed;
     Animator animator;
     private float triggerTarget;
     private float gripTarget;
@@ -19,14 +19,51 @@ public class Hand : MonoBehaviour
     private string animatorGripParam = "Grip";
     private string animatorTriggerParam = "Trigger";
 
+    // Physics Movement
+    [SerializeField] private GameObject followObject;
+    [SerializeField] private float followSpeed = 30f;
+    [SerializeField] private float rotateSpeed = 100f;
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private Vector3 rotationOffset;
+    private Transform followTarget;
+    private Rigidbody body;
+
     void Start()
     {
         animator = GetComponent<Animator>();
+
+        // Physics movement
+        followTarget = followObject.transform;
+        body = GetComponent<Rigidbody>();
+        body.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        body.interpolation = RigidbodyInterpolation.Interpolate;
+        body.mass = 20f;
+
+        // Teleport hands
+        body.position = followTarget.position;
+        body.rotation = followTarget.rotation;
     }
 
     void Update()
     {
         AnimateHand();
+
+        PhysicsMove();
+    }
+
+    private void PhysicsMove()
+    {
+        // Position
+        // update - fixed to use world space instead of local space: https://youtu.be/MYOjQICbd8I?t=604
+        var positionWithOffset = followTarget.position + positionOffset;
+        var distance = Vector3.Distance(positionWithOffset, transform.position);
+        body.velocity = (positionWithOffset - transform.position).normalized * (followSpeed * distance);
+
+        // Rotation (idk how this works, credit to video)
+        var rotationWithOffset = followTarget.rotation * Quaternion.Euler(rotationOffset);
+        var q = rotationWithOffset * Quaternion.Inverse(body.rotation); // difference between rotation of target (controller) and rotation of hand
+        q.ToAngleAxis(out float angle, out Vector3 axis);
+        body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
     }
 
     internal void SetGrip(float v)
@@ -43,12 +80,12 @@ public class Hand : MonoBehaviour
     {
         if (gripCurrent != gripTarget)
         {
-            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * animationSpeed);
             animator.SetFloat(animatorGripParam, gripCurrent);
         }
         if (triggerCurrent != triggerTarget)
         {
-            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * animationSpeed);
             animator.SetFloat(animatorTriggerParam, triggerCurrent);
         }
     }
--- VR Tennis/Assets/Scripts/Hands/HandController.cs
+++ VR Tennis/Assets/Scripts/Hands/HandController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+/**
+    Handles the getting the input values from the controller and pass to the Hand class to animate
+    https://youtu.be/DxKWq7z4Xao
+**/
 [RequireComponent(typeof(ActionBasedController))]
 public class HandController : MonoBehaviour
 {
